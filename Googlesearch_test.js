f3b3c30bb345a79298c2811d325f5b17dbb21754
vijayalakshmi-openturf <100668784+vijayalakshmi-openturf@users.noreply.github.com>
2022-03-01 17:35:53 +0530

    Add files via upload

---
+++ Googlesearch_test.js
@@ -0,0 +1,6 @@
+Feature('Googlesearch');
+
+Scenario('test something', ({ I }) => {
+I.amOnPage('/');
+I.wait(2);
+});
---
+++ codecept.conf.js
@@ -0,0 +1,31 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'https://www.google.com',
+      browser: 'chrome'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'Testcase-WDIO',
+  wdio:{
+    enabled:true,
+    services:['selenium-standalone']
+   
+  }
+
+}
